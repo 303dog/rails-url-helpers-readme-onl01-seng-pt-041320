3c58951a9504efa825e72b18d9cfef73deca9251
Paul_Beckwith <303dog@gmail.com>
2020-09-01 15:15:35 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: "Back to the future", description: "1.21 gigwatts!")
